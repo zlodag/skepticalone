31367208cd6faeb50f128732c6ad666d623e5a04
zlodag <zlodag@users.noreply.github.com>
2015-03-29 04:41:01 +1300

    updated hyperlink

--- hospital/_hospital.php
+++ hospital/_hospital.php
@@ -3,7 +3,7 @@
     <li><a href="betterpage">TextPage</a></li>
     <li><a href="report">Lab Reports</a></li>
     <li><a href="lab">Lab Display</a></li>
-    <li><a href="http://jsfiddle.net/zlodag/bwb0c6xc/embedded/result/">Non-Urgent Task Manager</a></li>
+    <li><a href="nutm">Non-Urgent Task Manager</a></li>
     <li>Electronic referrals</li>
     <li>Electronic lab requests</li>
 </ul>
