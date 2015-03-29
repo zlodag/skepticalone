ea3315edcf184c18905cf77ab1f30c4c9d316a55
zlodag <zlodag@users.noreply.github.com>
2015-03-29 20:03:20 +1300

    added time-based restriction to on call staff

--- hospital/nutm/amion.php
+++ hospital/nutm/amion.php
@@ -7,35 +7,49 @@
     </head>
     <body>
 <?php
-$file = fopen("data.csv","r");
-$last_specialty = "";
-$i = 0;
-$indices = [0, 1, 4];
+date_default_timezone_set("Pacific/Auckland");
+$time = new DateTime();
+echo '<p>' . $time->format(DATE_RSS) . '</p>';
+
 $database = [];
-echo("<table>");
+
+$file = fopen("data.csv","r");
 while(! feof($file)) {
     $r = fgetcsv($file);
     if (count($r) == 10) {
         $specialty = $r[0];
         if (!array_key_exists($specialty, $database)) {$database[$specialty] = [];}
-        $database[$specialty][$r[4]] = $r[1];
-        if ($specialty !== $last_specialty && $i !== 0) {
-            echo '</table><table>';
-            $last_specialty = $specialty;
-        }
-        echo '<tr>';
-        foreach($r as $index => $data) {
-            if (in_array($index, $indices)) {
-                echo '<td>' . $data . '</td>';
+        $database[$specialty][$r[4]] = [$r[1],$r[8],$r[9]];
+    }
+}
+fclose($file);
+
+function print_database() {
+    global $database;
+    global $time;
+    $timestr = $time->format('Hi');
+    foreach($database as $specialty => $rows) {
+        printf('<table><caption>%s</caption>', $specialty);
+        echo '<thead><th>Role</th><th>Person</th><th>Time</th></thead><tbody>';
+        foreach($rows as $role => $person_time) {
+            $person = $person_time[0];
+            if (
+                ($person_time[1] < $person_time[2] && $timestr >= $person_time[1] && $timestr <= $person_time[2])
+                || ($person_time[1] > $person_time[2] && ($timestr >= $person_time[1] || $timestr <= $person_time[2]))
+                || ($person_time[1] == $person_time[2])
+
+            ) {
+                $time = $person_time[1] . ' - ' . $person_time[2];
+                echo '<tr><td>' . $role . '</td><td>' . $person . '</td><td>' . $time . '</td></tr>';
             }
         }
-        echo '</tr>';
+        echo '</tbody></table>';
     }
-    $i++;
 }
-echo("</table>");
 
-fclose($file);
+
+
+print_database();
 ?>
     </body>
 </html>
