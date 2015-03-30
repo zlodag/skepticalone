a0f6bd94c1ff906bbe6221b52add8307b9247aea
zlodag <zlodag@users.noreply.github.com>
2015-03-30 23:00:21 +1300

    changed labviewer to link directly to main display panel

--- hospital/_hospital.php
+++ hospital/_hospital.php
@@ -2,7 +2,7 @@
     <li><a href="dc">Discharge Summary</a></li>
     <li><a href="betterpage">TextPage</a></li>
     <li><a href="report">Lab Reports</a></li>
-    <li><a href="lab">Lab Display</a></li>
+    <li><a href="lab/?nhi=uvw7654">Lab Display</a></li>
     <li><a href="nutm">Non-Urgent Task Manager</a></li>
     <li>Electronic referrals</li>
     <li>Electronic lab requests</li>
