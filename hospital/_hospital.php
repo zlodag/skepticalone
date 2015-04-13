c1c9c2d286be8d99c67eedec8faebacf39695ad9
zlodag <zlodag@users.noreply.github.com>
2015-04-14 02:30:57 +1200

    changed index

--- hospital/_hospital.php
+++ hospital/_hospital.php
@@ -3,7 +3,8 @@
     <li><a href="betterpage">TextPage</a></li>
     <li><a href="report">Lab Reports</a></li>
     <li><a href="lab/?nhi=uvw7654">Lab Display</a></li>
-    <li><a href="nutm">Non-Urgent Task Manager</a></li>
+    <li><a href="nutm1">Non-Urgent Task Manager v1 (demo)</a></li>
+    <li><a href="nutm">Non-Urgent Task Manager v2 (in progress)</a></li>
     <li>Electronic referrals</li>
     <li>Electronic lab requests</li>
 </ul>
