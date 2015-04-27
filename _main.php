314d6fc0e010563ea9e21480fcf369a124648710
zlodag <zlodag@users.noreply.github.com>
2015-04-27 12:58:58 +1200

    removed CC

--- _main.php
+++ _main.php
@@ -37,8 +37,6 @@ if (isset($target)) {
  src="http://www.w3.org/html/logo/downloads/HTML5_Badge_32.png" alt="HTML5 Validator" /></a>
 <a href="http://jigsaw.w3.org/css-validator/check/referer"><img 
  src="http://jigsaw.w3.org/css-validator/images/vcss" alt="CSS Validator" /></a>
-<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img
- alt="Creative Commons License" src="http://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a>
 </div>
 </body>
 </html>
