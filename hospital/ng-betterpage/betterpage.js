75273b2e6ce3be127a3d8a5d56417d01db6e2162
zlodag <zlodag@users.noreply.github.com>
2015-05-06 00:47:23 +1200

    validated betterpage both

--- hospital/ng-betterpage/betterpage.js
+++ hospital/ng-betterpage/betterpage.js
@@ -1,4 +1,5 @@
 (function() {
+    "use strict";
     var app = angular.module('betterpage', [])
     .config(['$locationProvider',function($locationProvider) {
         $locationProvider.html5Mode(true);
